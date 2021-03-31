8b3366a61ecfc382222c64cd14c260c4cd547757
Jake Wood <jake@jakewood.org>
2021-03-31 14:09:52 -0600

    Let's give this a whirl

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const github = require('@actions/github');
 
-
-// most @actions toolkit packages have async methods
 async function run() {
-  try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+  let repo = core.getInput('app') + '-';
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
+  if (github.context.eventName === 'workflow_dispatch') {
+    core.info('This job was fired by a workflow dispatch.');
+    core.info('Using the provided environment name...');
+    repo += core.getInput('environment');
   }
+  else {
+    repo += 'prod';
+  }
+
+  const layer = core.getInput('layer');
+  if (layer) {
+    repo += '-' + layer;
+  }
+
+  core.setOutput('repository', repo);
 }
 
 run();
